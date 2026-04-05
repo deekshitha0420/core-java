5c4cb2bb1f27aabf7445f9a9d2b6f2585a0104a9
Deekshitha <scpoornimapoornima@gmail.com>
2026-04-05 20:25:50 +0530

    Add files via upload

---
+++ ArrayRunner.java
@@ -0,0 +1,15 @@
+class ArrayRunner{
+	public static void main(String[] args){
+		
+		Array a = new Array();
+		a.StudentName="deekshitha";
+		a.Course="BCA";
+		a.rollNo=5;
+		a.College="Mandavya";
+		a.Percentage=92.05;
+		a.displayDetails();
+		 
+		
+	}
+
+}
