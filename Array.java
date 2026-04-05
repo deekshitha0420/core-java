3514381e75033ea7eea99071039b8af0d1f2768e
deekshitha0420 <deekshithad946@gmail.com>
2026-04-05 15:02:04 +0530

    test

---
+++ Array.java
@@ -0,0 +1,17 @@
+class Array{
+	
+	String StudentName;
+	String Course;
+	int rollNo;
+	String College;
+	double Percentage;
+	
+	public void displayDetails(){
+		System.out.println("StudentName:"+StudentName);
+		System.out.println("Course:"+Course);
+		System.out.println("rollNo:"+rollNo);
+		System.out.println("College:"+College);
+		System.out.println("Percentage:"+Percentage);
+	}
+
+}		
